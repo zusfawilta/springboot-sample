ee97fb70ba5e81d25ca9da81fa87b56b2d9025c2
zusfawilta <zusfawilta@gmail.com>
2024-12-02 17:09:20 +0700

    Initial Commit with Git Ignore

---
+++ src/main/java/com/example/springboot_sample/SpringbootSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSampleApplication.class, args);
+	}
+
+}
